6e74d99ccb6d8b0e8a5dff2f4e628e4e74c154ca
headbreakz <knighthero1005@gmail.com>
2020-06-10 11:44:38 +0900

    deleted

--- QT/test1.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import QPushButton, QStyle
-
-videoWidget = QVideoWidget()
-
-self.playButton = QPushButton()
-self.playButton.setEnabled(False)
-self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-self.playButton.clicked.connect(self.play)
